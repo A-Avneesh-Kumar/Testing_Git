304aa73fbd7dc09d5d6aeac1838f0105062e4f18
A-Avneesh-Kumar <a.avneeshkumar@gmail.com>
2023-09-26 17:23:41 +0530

    commited again

--- text1.py
+++ text1.py
@@ -1,11 +1,6 @@
 print("Avneesh kumar")
 print("Hello Someone change this code on github")
 print("Another Addition by developer_2")
-<<<<<<< HEAD
 print("line 4 printed by Local")
 print("line 5 printed by Local")
-=======
-print("line 4 printed by Github")
-print("line 5 printed by Github")
->>>>>>> 4da3c69b7fa235d9734cced8e35a436d98604fc7
 
