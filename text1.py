1cab8aa4bcb1a8c2cf59c9439b8e81033e8b3fd6
Avneesh kumar <a.avneeshkumar@gmail.com>
2023-09-26 16:43:03 +0530

    Update text1.py

--- text1.py
+++ text1.py
@@ -1,5 +1,4 @@
-<<<<<<< HEAD
 print("Avneesh kumar")
-=======
 print("Hello Someone change this code on github")
->>>>>>> c4047b40b707096f82730b20c88c8156848e1b7d
+print("Another Addition by developer_2")
+
